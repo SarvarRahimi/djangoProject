1f8501b78cdd326f4efec73db1c7a80e6d038498
Sarvar <sarvarrahimi111@gmail.com>
2024-12-24 17:53:30 +0330

    requirements file added and models modified

--- khakBardari/admin.py
+++ khakBardari/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Ranande
+from .models import Ranande, ServiceKhak
+
+# admin.site.register(Ranande)
 
 
 @admin.register(Ranande)
 class RanandeAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'mobile')
+
+
+@admin.register(ServiceKhak)
+class ServiceKhakAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ranande', 'mohandes')
--- khakBardari/models.py
+++ khakBardari/models.py
@@ -1,8 +1,10 @@
 import re
 
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -17,10 +19,23 @@ class Ranande(models.Model):
     ranande_id = models.CharField(max_length=30, unique=True, blank=True, null=True)
     first_name = models.CharField(max_length=50, blank=True, null=True)
     last_name = models.CharField(max_length=50, blank=True, null=True)
-    mobile = models.CharField(validators=[validate_phone],max_length=16, blank=True, null=True)
+    mobile = models.CharField(validators=[validate_phone], max_length=16, blank=True, null=True)
     logo = models.ImageField(upload_to='Driver_logo/',
                              default='Driver_logo/driver_default_logo.png',
                              validators=[FileExtensionValidator(allowed_extensions=['jpeg', 'jpg', 'png'])],
                              null=True, blank=True)
     car_type_name = models.CharField(max_length=50, blank=True, null=True)
     car_id = models.CharField(max_length=20, blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+
+
+class ServiceKhak(models.Model):
+    ranande = models.ForeignKey(Ranande, on_delete=models.PROTECT)
+    mohandes = models.ForeignKey(User, on_delete=models.PROTECT)
+    maghsad = models.CharField(max_length=50, blank=True, null=True)
+    date = models.DateTimeField(default=now())
+
+    def __str__(self):
+        return f'{self.date}'
